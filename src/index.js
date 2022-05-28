adae7b7b174bde00e09304ef087cbabb01f24b71
Alexander Rose <alexander.rose.11@my.csun.edu>
2022-05-28 00:54:28 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
+import {StrictMode} from "react";
 import App from './App';
 
-ReactDOM.render(<React.StrictMode><App/></React.StrictMode>, document.getElementById('root'));
+ReactDOM.render(<StrictMode><App/></StrictMode>, document.getElementById('root'))
